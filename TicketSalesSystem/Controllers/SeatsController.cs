e60b6c22eefad1550df1eaf637f75b095968e8bc
Liou <a47453386@gmail.com>
2026-02-10 07:18:02 +0800

    訂購畫面

--- TicketSalesSystem/Controllers/SeatsController.cs
+++ TicketSalesSystem/Controllers/SeatsController.cs
@@ -15,14 +15,45 @@ namespace TicketSalesSystem.Controllers
     {
         private readonly ISeatService _seatService;
         private readonly IBookingValidationService _bookingValidation;
+        private readonly TicketsContext _Context;
 
-        public SeatsController(ISeatService seatService, IBookingValidationService bookingValidation)
+        public SeatsController(ISeatService seatService, IBookingValidationService bookingValidation,TicketsContext context)
         {
             _seatService = seatService;
             _bookingValidation = bookingValidation;
+            _Context = context;
         }
 
         //使用者訂購頁面
+        public async Task<IActionResult> Index(string id) 
+        {
+            if (string.IsNullOrEmpty(id)) return NotFound();
+
+            //取得場次資訊
+            var session =await _Context.Session
+                .Include(s => s.Programme)
+                .Include(s => s.TicketsArea)
+                .FirstOrDefaultAsync(s => s.SessionID == id);
+            if (session == null) return NotFound();
+
+            //檢查是否狀態
+            if (session.Programme.ProgrammeStatusID !="O")
+            {
+                ViewData["Message"] = $"本活動目前狀態為：{session.Programme.ProgrammeStatus.ProgrammeStatusName}，暫不開放購票。";
+                return RedirectToAction("Index","Programmes");
+            }
+
+            //檢查是否在售票時間內
+            if(DateTime.Now<session.SaleStartTime||DateTime.Now>session.SaleEndTime)
+            {
+                TempData["Message"] = "本時段尚未開放購票";
+                return RedirectToAction("Index", "Programmes");
+            }
+            return View(session);
+
+        }
+
+
 
         //選擇場次(顯示)
         public async Task<IActionResult> IndexTest(string id)
